c9b3fad2527484e16c94d6e6aa37a7945bcaddaa
KhorevDmytro <djAiZer27>
2017-02-13 19:19:26 +0200

    add test Runner

--- src/test/java/com/playtika/common/Xmlparser.java
+++ src/test/java/com/playtika/common/Xmlparser.java
@@ -1,12 +1,20 @@
 package com.playtika.common;
 
+import com.playtika.tests.Test1;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -17,22 +25,55 @@ import java.util.Iterator;
 public class Xmlparser {
 
     @Test
-    public void parser() throws DocumentException {
+    public void testsGenerator() throws DocumentException, ClassNotFoundException {
         File xml = new File("/Users/dmitrijhorev/Documents/Repo2/playtika/src/test/java/com/playtika/suites/Regression1.xml");
         SAXReader reader = new SAXReader();
         Document doc = reader.read(xml);
         Element suite = doc.getRootElement();
         String suiteName = suite.attributeValue("name");
-
-        
-
         ArrayList<String> arrTests = parse(suite, suiteName);
         System.out.println(arrTests);
-        ArrayList<String> arrClasses = parse(suite, arrTests.get(0));
-        System.out.println(arrClasses);
-        ArrayList<String> methodsClasses = parse(suite, arrClasses.get(1));
-        System.out.println(methodsClasses);
+
+        for (int t = 0; t < arrTests.size(); t++) {
+            ArrayList<String> arrClasses = parse(suite, arrTests.get(t));
+
+            for (int c = 0; c < arrClasses.size(); c++) {
+                ArrayList<String> methodsClasses = parse(suite, arrClasses.get(c));
+
+                Class testClass = Class.forName("com.playtika.tests." + arrClasses.get(c));
+                if(methodsClasses.size() == 0) {
+                    Method[] methods = testClass.getDeclaredMethods();
+
+                    for (Method method : methods) {
+                        Annotation[] anns = method.getAnnotations();
+                        
+                        for (Annotation ann : anns){
+                            if (ann.annotationType().getName().equals("org.junit.Test")){
+                                testsRunner(testClass, method.getName());
+                            }
+                        }
+
+                    }
+                } else {
+                    for (int m = 0; m < methodsClasses.size(); m++) {
+                        testsRunner(testClass, methodsClasses.get(m));
+                    }
+
+                }
+            }
+        }
     }
+
+    private void testsRunner(Class cl, String method) throws ClassNotFoundException {
+        JUnitCore junit = new JUnitCore();
+        Request req = Request.method(cl, method);
+        Result result = junit.run(req);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        Assert.assertTrue(result.wasSuccessful());
+    }
+
 
     private ArrayList<String> parse(Element block, String nameParent) {
         ArrayList<String> arrNames = new ArrayList<String>();
